59b56b216ececd3d2fa42a5c55ac3151fc66fb06
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-08-06 12:22:23 +0200

    Expose Operator.Operation.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = {
+  Operation: require('./src/operation'),
   Compound: require('./src/compound'),
   ArrayOperation: require('./src/array_operation'),
   TextOperation: require('./src/text_operation'),
